f87adb88c528dc13061511db098e864bcb8731ff
Evan Chamberlain <evandchamberlain@gmail.com>
2022-07-08 14:56:18 -0700

    progress with interns on july 8th

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -18,39 +18,39 @@ class Shop {
 
   updateValue() {
     for (let i = 0; i < this.items.length; i++) {
-      if (!this.isItemMoreValuableWithAge(i)) {
-        if (this.items[i].value > MIN_ITEM_VALUE) {
-          if (this.doesItemDepreciate(i)) {
-            this.items[i].value -= 1;
+      var item = this.items[i];
+      if (!this.isItemMoreValuableWithAge(item)) {
+        if (item.value > MIN_ITEM_VALUE) {
+          if (!this.isLegendaryItem(item)) {
+            item.value -= 1;
           }
         }
       } else {
-        if (this.items[i].value < MAX_ITEM_VALUE) {
-          this.items[i].value += 1;
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            this.addUrgencyValueForBackstageDeadlines(i);
+        if (item.value < MAX_ITEM_VALUE) {
+          item.value += 1;
+          if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+            this.addUrgencyValueForBackstageDeadlines(item);
           }
         }
       }
-      if (this.doesItemDepreciate(i)) {
-        this.items[i].numberOfDaysToSell -= 1;
+      if (!this.isLegendaryItem(item)) {
+        item.numberOfDaysToSell -= 1;
       }
-      if (this.items[i].numberOfDaysToSell < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.doesItemStillHaveValue(i) && this.doesItemDepreciate(i)) {
-              this.items[i].value -= 1;
+      if (item.numberOfDaysToSell < 0) {
+        if (item.name != "Aged Brie") {
+          if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (
+              this.doesItemStillHaveValue(item) &&
+              !this.isLegendaryItem(item)
+            ) {
+              item.value -= 1;
             }
           } else {
-            this.items[i].value = MIN_ITEM_VALUE;
+            item.value = MIN_ITEM_VALUE;
           }
         } else {
-          if (this.items[i].value < MAX_ITEM_VALUE) {
-            this.items[i].value += 1;
+          if (item.value < MAX_ITEM_VALUE) {
+            item.value += 1;
           }
         }
       }
@@ -59,37 +59,37 @@ class Shop {
     return this.items;
   }
 
-  doesItemStillHaveValue(i) {
-    return this.items[i].value > MIN_ITEM_VALUE;
+  doesItemStillHaveValue(item) {
+    return item.value > MIN_ITEM_VALUE;
   }
 
-  addUrgencyValueForBackstageDeadlines(i) {
+  addUrgencyValueForBackstageDeadlines(item) {
     if (
-      this.items[i].numberOfDaysToSell <
+      item.numberOfDaysToSell <
       BACKSTAGE_PASS_DEADLINE_BEFORE_VALUE_INCREASE_BY_TWO
     ) {
-      if (this.items[i].value < MAX_ITEM_VALUE) {
-        this.items[i].value += 1;
+      if (item.value < MAX_ITEM_VALUE) {
+        item.value += 1;
       }
     }
     if (
-      this.items[i].numberOfDaysToSell <
+      item.numberOfDaysToSell <
       BACKSTAGE_PASS_DEADLINE_BEFORE_VALUE_INCREASE_BY_THREE
     ) {
-      if (this.items[i].value < MAX_ITEM_VALUE) {
-        this.items[i].value += 1;
+      if (item.value < MAX_ITEM_VALUE) {
+        item.value += 1;
       }
     }
   }
 
-  doesItemDepreciate(i) {
-    return this.items[i].name != "Sulfuras, Hand of Ragnaros";
+  isLegendaryItem(item) {
+    return item.name == "Sulfuras, Hand of Ragnaros";
   }
 
-  isItemMoreValuableWithAge(i) {
+  isItemMoreValuableWithAge(item) {
     return (
-      this.items[i].name == "Aged Brie" ||
-      this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
+      item.name == "Aged Brie" ||
+      item.name == "Backstage passes to a TAFKAL80ETC concert"
     );
   }
 }
